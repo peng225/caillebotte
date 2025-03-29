7c3267aeaf18d97fedf76c4bd095c38650f4c350
Shinya Hayashi <shinyaUT0925@gmail.com>
2025-03-29 14:06:03 +0900

    change some variable names to the camel case
    Signed-off-by: Shinya Hayashi <shinyaUT0925@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "./page.module.css";
 
-import thumb_chaos_game from "../public/images/chaos_game.png";
-import thumb_stereogram from "../public/images/stereogram.png";
-import thumb_hyperboloid from "../public/images/hyperboloid.png";
-import thumb_prime_spiral from "../public/images/prime_spiral.png";
-import thumb_voronoi_diagram from "../public/images/voronoi_diagram.png";
-import thumb_golden_angle from "../public/images/golden_angle.png";
-import thumb_koch_curve from "../public/images/koch_curve.png";
-import thumb_sampling_theorem from "../public/images/sampling_theorem.png";
+import thumbChaosGame from "../public/images/chaos_game.png";
+import thumbStereogram from "../public/images/stereogram.png";
+import thumbHyperboloid from "../public/images/hyperboloid.png";
+import thumbPrimeSpiral from "../public/images/prime_spiral.png";
+import thumbVoronoiDiagram from "../public/images/voronoi_diagram.png";
+import thumbGoldenAngle from "../public/images/golden_angle.png";
+import thumbKochCurve from "../public/images/koch_curve.png";
+import thumbSamplingTheorem from "../public/images/sampling_theorem.png";
 
 export default function Home() {
   return (
@@ -17,7 +17,7 @@ export default function Home() {
       <section
         className={styles.hero}
         style={{
-          background: `url(${thumb_chaos_game.src})`,
+          background: `url(${thumbChaosGame.src})`,
           backgroundRepeat: "no-repeat",
           backgroundPosition: "center center",
           backgroundSize: "cover",
@@ -33,49 +33,49 @@ export default function Home() {
         <div className={styles.artLink}>
           <div className={styles.artCard}>
             <Link href={"/work/chaos_game"}>
-              <Image src={thumb_chaos_game} alt="Chaos Game" />
+              <Image src={thumbChaosGame} alt="Chaos Game" />
               <p>Chaos Game</p>
             </Link>
           </div>
           <div className={styles.artCard}>
             <Link href={"/work/stereogram"}>
-              <Image src={thumb_stereogram} alt="Stereogram" />
+              <Image src={thumbStereogram} alt="Stereogram" />
               <p>Stereogram</p>
             </Link>
           </div>
           <div className={styles.artCard}>
             <Link href={"/work/hyperboloid"}>
-              <Image src={thumb_hyperboloid} alt="Hyperboloid" />
+              <Image src={thumbHyperboloid} alt="Hyperboloid" />
               <p>Hyperboloid</p>
             </Link>
           </div>
           <div className={styles.artCard}>
             <Link href={"/work/prime_spiral"}>
-              <Image src={thumb_prime_spiral} alt="Prime Spiral" />
+              <Image src={thumbPrimeSpiral} alt="Prime Spiral" />
               <p>Prime Spiral</p>
             </Link>
           </div>
           <div className={styles.artCard}>
             <Link href={"/work/voronoi_diagram"}>
-              <Image src={thumb_voronoi_diagram} alt="Voronoi Diagram" />
+              <Image src={thumbVoronoiDiagram} alt="Voronoi Diagram" />
               <p>Voronoi Diagram</p>
             </Link>
           </div>
           <div className={styles.artCard}>
             <Link href={"/work/golden_angle"}>
-              <Image src={thumb_golden_angle} alt="Golden Angle" />
+              <Image src={thumbGoldenAngle} alt="Golden Angle" />
               <p>Golden Angle</p>
             </Link>
           </div>
           <div className={styles.artCard}>
             <Link href={"/work/koch_curve"}>
-              <Image src={thumb_koch_curve} alt="Koch Curve" />
+              <Image src={thumbKochCurve} alt="Koch Curve" />
               <p>Koch Curve</p>
             </Link>
           </div>
           <div className={styles.artCard}>
             <Link href={"/work/sampling_theorem"}>
-              <Image src={thumb_sampling_theorem} alt="Sampling Theorem" />
+              <Image src={thumbSamplingTheorem} alt="Sampling Theorem" />
               <p>Sampling Theorem</p>
             </Link>
           </div>
